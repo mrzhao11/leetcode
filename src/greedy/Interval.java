1791bd5140046cb4734a09d04f0de5dc8504a00e
Zhao Yisen <zhaoyisen119@163.com>
2025-11-24 15:51:37 +0800

    greedy

---
+++ src/greedy/Interval.java
@@ -0,0 +1,99 @@
+package greedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Interval {
+    // 452. 用最小数量的箭引爆气球
+    // 给你一些气球，气球用区间表示，区间的开始和结束分别代表水平方向上气球的起始和结束坐标。
+    // 由于水平方向上没有限制，气球的直径可以看作一个区间。
+    // 一支箭可以沿着 x 轴从不同点完全穿过气球所在的区间来引爆气球。
+    // 给你一个数组 points ，其中 points[i] = [xstart, xend] 表示水平直径在 xstart 和 xend 之间的气球。
+    // 求引爆所有气球所需的最小箭数。 可以射出的箭的数量没有限制，且可以放置在 x 轴上的任意位置。
+    public static int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0])); // 按区间起点排序
+        int count = 1;
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > points[i - 1][1]) {
+                count++;
+            } else {
+                points[i][1] = Math.min(points[i][1], points[i - 1][1]); // 更新重叠区间右边边界的最小值
+            }
+        }
+        return count;
+    }
+
+    public static int earseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1])); // 按区间结束位置排序
+        int remove = 0;  // 记录需要移除的区间数量
+        int pre = intervals[0][1]; // 记录上一个区间的结束位置
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < pre) { // 当前区间与上一个区间重叠
+                remove++;
+                pre = Math.min(pre, intervals[i][1]); // 更新重叠区间右边边界的最小值
+            } else {
+                pre = intervals[i][1];
+            }
+        }
+        return remove;
+    }
+
+    // 合并区间
+    // 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+    // 请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+    public static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0])); // 按区间起点排序
+        List<int[]> res = new ArrayList<>();
+        int start = intervals[0][0]; // 记录当前合并区间的起点
+        int rightmostRightBound = intervals[0][1]; // 记录当前合并区间的右边界
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] > rightmostRightBound) { // 当前区间与合并区间不重叠
+                res.add(new int[]{start, rightmostRightBound}); // 将合并区间加入结果列表
+                start = intervals[i][0];
+                rightmostRightBound = intervals[i][1];
+            } else { // 当前区间与合并区间重叠
+                rightmostRightBound = Math.max(rightmostRightBound, intervals[i][1]);
+            }
+        }
+        res.add(new int[]{start, rightmostRightBound}); // 加入最后一个合并区间
+        return res.toArray(new int[res.size()][]);
+    }
+
+    // 划分字母区间
+    // 字符串 s 由小写英文字母组成。我们要把这个字符串划分为尽可能多的片段，
+    // 使得每个字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
+    public static List<Integer> partitionLabels(String s) {
+        List<Integer> res = new ArrayList<>();
+        int[] end = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            end[s.charAt(i) - 'a'] = i; // 记录每个字母最后出现的位置
+        }
+        int left = 0, right = 0; // 记录当前片段的左右边界
+        for (int i = 0; i < s.length(); i++) {
+            right = Math.max(right, end[s.charAt(i) - 'a']); // 更新右边界
+            if (i == right) { // 到达当前片段的右边界
+                res.add(right - left + 1); // 计算片段长度并加入结果列表
+                left = i + 1; // 更新左边界
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] points = {{10,16},{2,8},{1,6},{7,12}};
+        System.out.println(findMinArrowShots(points)); // 输出 2
+
+        int[][] intervals = {{1,2},{2,3},{3,4},{1,3}};
+        System.out.println(earseOverlapIntervals(intervals)); // 输出 1
+
+        int[][] mergeIntervals = {{1,3},{2,6},{8,10},{15,18}};
+        int[][] merged = merge(mergeIntervals);
+        for (int[] interval : merged) {
+            System.out.println(Arrays.toString(interval)); // 输出 [1,6], [8,10], [15,18]
+        }
+
+        String s = "ababcbacadefegdehijhklij";
+        System.out.println(partitionLabels(s)); // 输出 [9,7,8]
+    }
+}
