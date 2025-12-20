667c7319d853055307f3daf8fc471f8e44ae09aa
Zhao Yisen <zhaoyisen119@163.com>
2025-12-20 22:58:16 +0800

    string

---
+++ src/string/FindAllAnagramsinString.java
@@ -0,0 +1,61 @@
+package string;
+
+import java.util.*;
+
+public class FindAllAnagramsinString {
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        if (s.length() < p.length())
+            return res;
+        // Map存放 p 中每个字符及其出现的次数
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : p.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int l = 0, r = 0;
+        int need = p.length(); // 还需要匹配的字符数
+
+        // 维护一个滑动窗口 [l,r]，始终保持窗口长度小于等于p.length()，map中记录的是：p还缺少的字符数
+        // 初始map = p 的字符频率
+        // 窗口右移时，如果遇到 p 中的字符，说明匹配到了一个需要的字符，need 减 1，map 中该字符的频率减 1
+        // 当窗口长度等于 p.length() 时，判断 need 是否为 0，如果是，则记录起始索引 l
+        // 然后窗口左移，若移出的字符在 p 中，说明失去匹配了一个需要的字符，need 加 1，map 中该字符的频率加 1
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            // 当前字符在 p 中，消耗一个需要匹配的字符，need 减 1
+            if (map.containsKey(c)) {
+                // 大于0判断加入前是否是需要的字符
+                if (map.get(c) > 0)
+                    need--;
+                map.put(c, map.get(c) - 1);
+            }
+            r++;
+
+            // 当窗口长度等于 p.length() 时，检查是否找到一个异位词
+            if (r - l == p.length()) {
+                if (need == 0) res.add(l); // 找到一个异位词，记录起始索引
+                char leftChar = s.charAt(l);
+                if (map.containsKey(leftChar)) {
+                    // 大于等于0判断移出后是否是需要的字符
+                    if (map.get(leftChar) >= 0)
+                        need++;
+                    map.put(leftChar, map.get(leftChar) + 1);
+                }
+                l++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 输入字符串 s
+        String s = sc.nextLine();
+        // 输入字符串 p
+        String p = sc.nextLine();
+
+        List<Integer> result = findAnagrams(s, p);
+        System.out.println(result);
+    }
+}
---
+++ src/string/LongestSubstringNoRepeat.java
@@ -0,0 +1,31 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringNoRepeat {
+    // 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+    public static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>(); // 存储字符及其最新出现的位置
+        int res = 0;
+
+        // 维护一个滑动窗口 [l, r]
+        // 当右指针遇到重复字符时，窗口需要恢复到无重复，左指针跳到重复字符上一次出现位置的下一个位置
+        // 每一步都更新最长长度
+        for (int l = 0, r = 0; r < s.length(); r++) {
+            char c = s.charAt(r);
+
+            // 如果有重复有可能在窗口内，也可能在窗口外
+            if (map.containsKey(c)) {
+                // 字符c重复，保证c在窗口内只有一个，需要更新左指针位置
+                // 左指针只能右移，不能左移，因此这个重复字符可能已经排除在窗口外了，所以取最大值
+                l = Math.max(l, map.get(c) + 1);
+            }
+
+            map.put(c, r);
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+}
