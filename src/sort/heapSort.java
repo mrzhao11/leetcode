42f2981de36038e2566a20720e9ec1a7d3908c27
Zhao Yisen <zhaoyisen119@163.com>
2026-01-30 00:15:07 +0800

    sort

--- src/sort/heapSort.java
+++ src/sort/heapSort.java
@@ -2,11 +2,13 @@ package sort;
 
 public class heapSort {
     // 堆排序
+    // 时间复杂度 O(nlogn)，空间复杂度 O(1)，不稳定排序
+    // 利用完全二叉树构成的 大顶堆，不断把 最大值（堆顶）换到数组末尾，再对剩余部分重新维护堆结构。
     public static int[] heapSort(int[] arr) {
         if (arr.length < 2) return arr;
         heapify(arr, arr.length - 1); // 构建大顶堆
         for (int i = arr.length - 1; i > 0; i--) {
-            swap.swap(arr, 0, i);        // 最大值（堆顶）换到未排序部分的最后
+            swap.swap(arr, 0, i);        // 堆顶和末尾交换，这个时候后面是有序的了，只需对前面部分堆化
             siftDown(arr, 0, i - 1);     // 对 [0, i-1] 再次堆化（只需要从堆顶下滤）
         }
         return arr;
@@ -20,17 +22,21 @@ public class heapSort {
         }
     }
 
-    // 下滤方法
+    // 下滤方法, hole 是要下滤的结点下标，r 是最后一个元素下标，把 hole 处的元素下滤到合适位置
+    // 下滤结束后，区间 [0, r] 满足堆序
     private static void siftDown(int[] arr, int hole, int r) {
         int target = arr[hole], child = hole * 2 + 1; // target 是要下滤的结点的值
         while(child <= r) { // child 最大为 r
-            if (child < r && arr[child + 1] > arr[child]) child++; // 选出左右孩子更大的那个
+            // 如果右孩子存在，且右孩子大于左孩子，则 child 指向右孩子
+            if (child < r && arr[child + 1] > arr[child])
+                child++; // 选出较大的孩子结点
             if (arr[child] > target) { // 若 child 大于 target
                 arr[hole] = arr[child]; // 则 arr[child] 上移到下标 hole 处
                 hole = child; // hole 更新为 child (下滤)
                 child = hole * 2 + 1; // 更新 child ，也可以写成 child = child * 2 + 1
             } else break; // 若 arr[child] <= target ，说明下标 hole 处已经满足堆序，退出 while
         }
-        arr[hole] = target; // 将 target 填 入hole 中
+        // 下滤结束后，hole 处是空穴，放入 target
+        arr[hole] = target;
     }
 }
--- src/sort/mergeSort.java
+++ src/sort/mergeSort.java
@@ -1,9 +1,12 @@
 package sort;
 
 public class mergeSort {
-    public static int[] mergeSort(int[] arr) {
+    // 归并排序
+    // 时间复杂度 O(nlogn)，空间复杂度 O(n)，稳定排序
+    // 采用分治思想，将数组不断二分，直到子数组长度为1，然后合并有序子数组
+    public static int[] mergeSort1(int[] arr) {
         if (arr.length < 2) return arr;
-        int[] tmpArr = new int[arr.length];
+        int[] tmpArr = new int[arr.length]; // 辅助数组，用于合并时存放有序元素
         mergeSort(arr, tmpArr, 0, arr.length - 1);
         return arr;
     }
@@ -13,14 +16,16 @@ public class mergeSort {
             int c = l + (r - l) / 2;
             mergeSort(arr, tmpArr, l, c); // 排左半部分
             mergeSort(arr, tmpArr, c + 1, r); // 排右半部分
-            merge(arr, tmpArr, l, c, r);
+            merge(arr, tmpArr, l, c, r); // 合并两个有序部分
         }
     }
     // 非原地合并方法，假设左半和右半已经有序，此时合并两个有序段
     private static void merge(int[] arr, int[] tmpArr, int l, int c, int r) {
         int lh = l, rh = c + 1, h = l; // lh: left head, rh: right head, h: tmpArr head
+        // 比较左右两半的元素，依次放入 tmpArr 中
         while (lh <= c && rh <= r) {
-            tmpArr[h++] = arr[lh] <= arr[rh] ? arr[lh++] : arr[rh++];
+            if (arr[lh] <=  arr[rh]) tmpArr[h++] = arr[lh++]; // 左半边元素较小，放入 tmpArr
+            else tmpArr[h++] = arr[rh++]; // 右半边元素较小，放入 tmpArr
         }
         while (lh <= c) tmpArr[h++] = arr[lh++]; // 左半边还有剩余，加入 tmpArr 末尾
         while (rh <= r) tmpArr[h++] = arr[rh++]; // 右半边还有剩余，加入 tmpArr 末尾
