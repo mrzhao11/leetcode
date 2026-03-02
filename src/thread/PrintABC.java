4e34ba95329ac4f22835124ef71b0ecc5c03eefa
Zhao Yisen <zhaoyisen119@163.com>
2026-03-02 21:44:23 +0800

    codetop

--- src/thread/PrintABC.java
+++ src/thread/PrintABC.java
@@ -15,15 +15,17 @@ public class PrintABC {
         new Thread(()->printer.print('B',printer.semB,printer.semC)).start(); // 线程B打印，获取semB，释放semC
         new Thread(()->printer.print('C',printer.semC,printer.semA)).start(); // 线程C打印，获取semC，释放semA
     }
+
+    // 打印方法，参数ch表示要打印的字符，current表示当前线程需要获取的信号量，next表示下一个线程需要释放的信号量
     public void print(char ch, Semaphore current, Semaphore next) {
         try {
             for (int i = 0; i < n; i++) {
                 current.acquire();  // 获取当前信号量
-                System.out.println(Thread.currentThread().getName() + ": " + ch);
+                System.out.println(Thread.currentThread().getName() + ": " + ch); // 打印字符
                 next.release();     // 释放下一个信号量
             }
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // 处理线程中断异常
         }
     }
 }
